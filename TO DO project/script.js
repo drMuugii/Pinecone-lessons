d626f882872fbabaa8f291cd5cb08cb476676fa5
drMuugii <dr.muuguu@gmail.com>
2023-12-27 19:25:19 +0800

    12/27 TODO

---
+++ TO DO project/script.js
@@ -0,0 +1,29 @@
+function showModal() {
+  const modal = document.getElementById("modaId");
+  modal.style.display = "flex";
+}
+
+function hideModal() {
+  const modal = document.getElementById("modaId");
+  modal.style.display = "none";
+}
+
+function addTask() {
+  const title = document.getElementById("taskTitle").value;
+  const description = document.getElementById("taskDescription").value;
+  const taskHtml = `<div class="task">${title}(${description})</div>`;
+  const todoElement = document.getElementById("todoTasks");
+
+  todoElement.innerHTML = todoElement.innerHTML + taskHtml;
+
+  hideModal();
+  resetFrom();
+}
+
+function resetFrom() {
+  document.getElementById("taskTitle").value = "";
+  document.getElementById("taskDescription").value = "";
+}
+
+const font = (document.getElementsByTagName("p")[1].style.fontFamily =
+  "Montserrat, Arial, sans-serif");
