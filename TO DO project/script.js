3a8979a2d9153ab1b4781f3ec874ee93fabbc60a
drMuugii <dr.muuguu@gmail.com>
2023-12-28 19:36:27 +0800

    12.28

--- TO DO project/script.js
+++ TO DO project/script.js
@@ -1,29 +1,63 @@
-function showModal() {
-  const modal = document.getElementById("modaId");
+function showModal(targetId) {
+  const modal = document.querySelector("#createModal");
   modal.style.display = "flex";
+  currentTarget = targetId;
 }
 
 function hideModal() {
-  const modal = document.getElementById("modaId");
+  const modal = document.getElementById("createModal");
   modal.style.display = "none";
 }
+
+let currentTarget = "todoTasks";
+let generateId = 0;
 
 function addTask() {
   const title = document.getElementById("taskTitle").value;
   const description = document.getElementById("taskDescription").value;
-  const taskHtml = `<div class="task">${title}(${description})</div>`;
-  const todoElement = document.getElementById("todoTasks");
+  const priority = document.getElementById("taskPriority").value;
+
+  generateId++;
+
+  //! fles column bolgoh
+  const taskHtml = `<div class="task"
+  id="task-${generateId}">
+  <div class="taskHtml"> <i class="fa-solid fa-circle-check"></i> </div>
+  <div>${title}</div>
+  <div> <i class="fa-regular fa-circle-xmark"></i></div>
+
+
+  ${description}<br>
+  ${priority} <br>
+  <button onclick="removeTask('task-${generateId}')">
+  remove task
+  </button>
+  </div>`;
+
+  const todoElement = document.getElementById(currentTarget);
 
   todoElement.innerHTML = todoElement.innerHTML + taskHtml;
 
   hideModal();
   resetFrom();
+
+  // if (title === "") {
+  //   alert("To do ---> Title ee oruulna uu");
+  // }
+  // if (description === "") {
+  //   alert("To do ---> Description ee oruulna uu");
+  // } else {
+  //   todoElement.innerHTML = todoElement.innerHTML + taskHtml;
+  //   hideModal();
+  //   resetFrom();
+  // }
+}
+
+function removeTask(taskId) {
+  document.getElementById(taskId).remove();
 }
 
 function resetFrom() {
   document.getElementById("taskTitle").value = "";
   document.getElementById("taskDescription").value = "";
 }
-
-var font = (document.getElementsByTagName("p")[0].style.fontFamily =
-  "Montserrat, Arial, sans-serif");
